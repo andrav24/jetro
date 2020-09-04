970cebb41b95252ac3d39d1b7ab1157d78087854
andrav24 <andrav24@yandex.ru>
2020-09-04 20:56:51 +0700

    add visibilaty for arrow-prev

--- js/main.js
+++ js/main.js
@@ -4,16 +4,15 @@ $(function(){
     slidesToScroll: 1,
     arrows: true,
     fade: true,
-    asNavFor: '.slider-nav'
+    asNavFor: '.slider-nav',
   });
   $('.slider-nav').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
     dots: false,
-    centerMode: true,
     focusOnSelect: true,
+    variableWidth: true,
     centerPadding: '0px'
   });
 });
-
